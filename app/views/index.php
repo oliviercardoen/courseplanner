30a88c2536aaeb7a1fbfd3267d4a98abeba02c8d
Olivier Cardoen <olivier.cardoen@octopix.be>
2014-06-14 15:46:36 +0200

    Refactor default views.

--- app/views/index.php
+++ app/views/index.php
@@ -1,1 +1,7 @@
-<p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+<?php include dirname(__FILE__) . '/../alert.php'; ?>
+
+<?php if ( isset( $title ) ):?>
+	<h1 class="page-header"><?php echo $title; ?></h1>
+<?php endif; ?>
+
+<p class="lead"><?php echo ( isset( $content ) ) ? $content : ''; ?></p>
--- app/views/layout.php
+++ app/views/layout.php
@@ -29,22 +29,21 @@
 				</button>
 				<a class="navbar-brand" href="<?php echo \App\App::url( 'home' ); ?>">Course Planner</a>
 			</div>
-			<div class="collapse navbar-collapse">
-				<ul class="nav navbar-nav">
-					<li><a href="<?php echo \App\App::url( 'home' ); ?>">Accueil</a></li>
-					<li><a href="<?php echo \App\App::url( 'courses' ); ?>">Mes Cours</a></li>
-					<li><a href="<?php echo \App\App::url( 'curriculums' ); ?>">Mes Formations</a></li>
-				</ul>
-			</div><!--/.nav-collapse -->
-		</div>
-	</div>
+			<?php if ( \App\App::hasUser() ): ?>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="<?php echo \App\App::url( 'home' ); ?>">Accueil</a></li>
+						<li><a href="<?php echo \App\App::url( 'courses' ); ?>">Mes Cours</a></li>
+						<li><a href="<?php echo \App\App::url( 'curriculums' ); ?>">Mes Formations</a></li>
+					</ul><!-- .nav -->
+				</div><!--/.nav-collapse -->
+			<?php endif; ?>
+		</div><!-- .container -->
+	</div><!-- .navbar -->
 
 	<div class="site container">
 
 		<div id="main" class="site-main">
-			<?php if ( isset( $title ) ):?>
-				<h1 class="page-header"><?php echo  $title; ?></h1>
-			<?php endif; ?>
 			<?php echo ( isset( $content ) ) ? $content : 'Page content is empty.'; ?>
 		</div>
 
