2027831bd97774192f651c997fc4304f8907d924
Olivier Cardoen <olivier.cardoen@octopix.be>
2014-06-14 16:29:00 +0200

    Improve responsiveness of index of courses and curriculums.

--- app/views/courses/index.php
+++ app/views/courses/index.php
@@ -1,31 +1,31 @@
 <?php include dirname(__FILE__) . '/../alert.php'; ?>
 
 <div class="row">
-	<div class="col-sm-10">
+	<div class="col-sm-10 col-xs-9">
 		<h1 class="page-header"><?php echo $title; ?></h1>
 	</div>
-	<div class="col-sm-2">
-		<a class="pull-right btn btn-default" href="<?php echo \App\App::url( 'courses' ) . '/new/'; ?>"><i class="glyphicon glyphicon-plus mr-xxsmall"></i>Ajouter</a>
+	<div class="col-sm-2 col-xs-3">
+		<a class="pull-right btn btn-default" href="<?php echo \App\App::url( 'courses' ) . '/new/'; ?>"><i class="glyphicon glyphicon-plus mr-xxsmall"></i><span class="hidden-xs">Ajouter</span></a>
 	</div>
 </div>
 <?php if ( !empty( $courses ) ): ?>
 	<table class="table">
 		<tr>
-			<th>Code</th>
+			<th class="hidden-xs">Code</th>
 			<th>Nom du cours</th>
-			<th>Début</th>
-			<th>Fin</th>
-			<th>Annexes</th>
-			<th></th>
+			<th class="hidden-xs">Début</th>
+			<th class="hidden-xs">Fin</th>
+			<th class="hidden-xs">Annexes</th>
+			<th class="hidden-xs"></th>
 		</tr>
 		<?php foreach( $courses as $course ): ?>
 			<tr>
-				<td><?php echo $course->code; ?></td>
+				<td class="hidden-xs"><?php echo $course->code; ?></td>
 				<td><a href="<?php echo \App\App::url( 'courses' ) . '/show/' . $course->id; ?>"><?php echo $course->name; ?></a></td>
-				<td><?php echo $course->start_date; ?></td>
-				<td><?php echo $course->end_date; ?></td>
-				<td><a href="<?php echo $course->reference_document; ?>" target="_blank">Dossier pédagogique<i class="glyphicon glyphicon-share-alt ml-xxsmall"></i></a></td>
-				<td class="text-right">
+				<td class="hidden-xs"><?php echo $course->start_date; ?></td>
+				<td class="hidden-xs"><?php echo $course->end_date; ?></td>
+				<td class="hidden-xs"><a href="<?php echo $course->reference_document; ?>" target="_blank">Dossier pédagogique<i class="glyphicon glyphicon-share-alt ml-xxsmall"></i></a></td>
+				<td class="text-right hidden-xs">
 					<a class="btn btn-default btn-sm ml-xxsmall" href="<?php echo \App\App::url( 'courses' ) . '/edit/' . $course->id; ?>"><i class="glyphicon glyphicon-pencil"></i></a>
 					<form class="inline-block" action="<?php echo \App\App::url( 'courses' ) . '/delete/'; ?>" method="POST">
 						<input type="hidden" name="id" value="<?php echo $course->id; ?>" />
--- app/views/curriculums/index.php
+++ app/views/curriculums/index.php
@@ -1,25 +1,25 @@
 <?php include dirname(__FILE__) . '/../alert.php'; ?>
 
 <div class="row">
-	<div class="col-sm-10">
+	<div class="col-sm-10 col-xs-9">
 		<h1 class="page-header"><?php echo $title; ?></h1>
 	</div>
-	<div class="col-sm-2">
-		<a class="pull-right btn btn-default" href="<?php echo \App\App::url( 'curriculums' ) . '/new/'; ?>"><i class="glyphicon glyphicon-plus mr-xxsmall"></i>Ajouter</a>
+	<div class="col-sm-2 col-xs-3">
+		<a class="pull-right btn btn-default" href="<?php echo \App\App::url( 'curriculums' ) . '/new/'; ?>"><i class="glyphicon glyphicon-plus mr-xxsmall"></i><span class="hidden-xs">Ajouter</span></a>
 	</div>
 </div>
 <?php if ( !empty( $entities ) ): ?>
 	<table class="table">
 		<tr>
 			<th>Nom de la formation</th>
-			<th>Période</th>
-			<th></th>
+			<th class="hidden-xs">Période</th>
+			<th class="hidden-xs"></th>
 		</tr>
 		<?php foreach( $entities as $entity ): ?>
 			<tr>
 				<td><a href="<?php echo \App\App::url( 'curriculums' ) . '/show/' . $entity->id; ?>"><?php echo $entity->name; ?></a></td>
-				<td><?php echo $entity->timeslot()->name; ?></td>
-				<td class="text-right">
+				<td class="hidden-xs"><?php echo $entity->timeslot()->name; ?></td>
+				<td class="text-right hidden-xs">
 					<a class="btn btn-default btn-sm ml-xxsmall" href="<?php echo \App\App::url( 'curriculums' ) . '/edit/' . $entity->id; ?>"><i class="glyphicon glyphicon-pencil"></i></a>
 					<form class="inline-block" action="<?php echo \App\App::url( 'curriculums' ) . '/delete/'; ?>" method="POST">
 						<input type="hidden" name="id" value="<?php echo $entity->id; ?>" />
