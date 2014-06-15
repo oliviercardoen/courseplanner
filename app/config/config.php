d967aab2b5f3645f7b668fd97d0bf7d81cf43fdb
Olivier Cardoen <olivier.cardoen@octopix.be>
2014-06-15 18:38:49 +0200

    Add salt in app config.

--- app/config/config.php
+++ app/config/config.php
@@ -1,11 +1,13 @@
 <?php
 $config = array(
 	'menu' => array(
-		'home'        => 'http://sites/php/courseplanner',
-		'courses'     => 'http://sites/php/courseplanner/courses',
-		'curriculums' => 'http://sites/php/courseplanner/curriculums',
-		'register'    => 'http://sites/php/courseplanner/register',
-		'logout'      => 'http://sites/php/courseplanner/logout',
-		'profile'     => 'http://sites/php/courseplanner/profile',
-	)
+		'home'         => 'http://sites/php/courseplanner',
+		'courses'      => 'http://sites/php/courseplanner/courses',
+		'curriculums'  => 'http://sites/php/courseplanner/curriculums',
+		'register'     => 'http://sites/php/courseplanner/register',
+		'authenticate' => 'http://sites/php/courseplanner/authenticate',
+		'logout'       => 'http://sites/php/courseplanner/logout',
+		'profile'      => 'http://sites/php/courseplanner/profile',
+	),
+	'salt' => sha1( 'courseplanner' )
 );
